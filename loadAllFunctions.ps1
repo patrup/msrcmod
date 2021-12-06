0980e39cb86778957eaf0302cea387e413324903
Patrick Rupprecht <Patrick.Rupprecht@advellence.com>
2021-12-06 20:08:21 +0100

    fixed slashes in URL

--- loadAllFunctions.ps1
+++ loadAllFunctions.ps1
@@ -1,7 +1,3 @@
 $exclude = @("loadAllFunctions.ps1","MsrcSecurityUpdates.testsMod.ps1")
 
-# get-childitem .\utilities | Select-Object -ExpandProperty FullName | ForEach-Object {$exclude += $_.ToString()}
-
-Get-ChildItem -Recurse -Path '*.ps1' -Exclude $exclude | Select-Object -ExpandProperty FullName | Where-Object {$_ -notlike "*utilities*"} | ForEach-Object {& $_}
-
-# & {.\Private\ParseJsonString.ps1}||$ScriptRoute = [System.IO.Path]::GetFullPath([System.IO.Path]::Combine($PSScriptRoot, "Scriptname.ps1"))
+Get-ChildItem -Recurse -Path '*.ps1' -Exclude $exclude | Resolve-Path -Relative | Where-Object {$_ -notlike "*utilities*"} | ForEach-Object {& $_}
