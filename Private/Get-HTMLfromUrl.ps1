51a436df4afbee934da88dcf1e115b6bc9184bfb
Patrick Rupprecht <Patrick.Rupprecht@advellence.com>
2021-12-07 07:30:44 +0100

    include HAP

--- Private/Get-HTMLfromUrl.ps1
+++ Private/Get-HTMLfromUrl.ps1
@@ -1,22 +1,21 @@
-$scriptroot = "$PSScriptRoot\.."
-Add-Type -Path "$scriptroot\lib\Net45\HtmlAgilityPack.dll"
+$scriptpath = $PSScriptRoot -split "Private"
+$libpath = $scriptpath[0] + "lib\Net45\HtmlAgilityPack.dll"
 
-function Get-HTMLfromUrl{
-    Param (
-        [Parameter(Mandatory,ValueFromPipeline)]
-        [string]$url
-    )
+Add-Type -Path $libpath
+Write-Host $libpath
 
+function Get-HTMLfromUrl($url) {
+   
     $web = New-Object HtmlAgilityPack.HtmlWeb
 
-$response = $web.LoadFromWebAsync($url)
-$improvements = $response.Result.DocumentNode.SelectNodes("//section[contains(@aria-label,'Improvements')]")
-$issues = $response.Result.DocumentNode.SelectNodes("//section[contains(@aria-label,'Issues')]")
+    $response = $web.LoadFromWebAsync($url)
+    $improvements = $response.Result.DocumentNode.SelectNodes("//section[contains(@aria-label,'Improvements')]")
+    $issues = $response.Result.DocumentNode.SelectNodes("//section[contains(@aria-label,'Issues')]")
 
-$kbarticle = [PSCustomObject]@{
-    Improvements = $improvements;
-    Issues = $issues
-}
+    $kbarticle = [PSCustomObject]@{
+        Improvements = $improvements;
+        Issues       = $issues
+    }
 
-return $kbarticle
+    return $kbarticle
 }
